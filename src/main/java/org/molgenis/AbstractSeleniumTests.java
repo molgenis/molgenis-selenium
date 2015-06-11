65674be235f5e9f7136e1e72f161024521981454
jjettenn <jjettenn@gmail.com>
2015-06-11 15:14:41 +0200

    Close browser windows after executing test

--- src/main/java/org/molgenis/AbstractSeleniumTests.java
+++ src/main/java/org/molgenis/AbstractSeleniumTests.java
@@ -1,31 +1,22 @@
 package org.molgenis;
 
-import static org.testng.Assert.fail;
-
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 
 @ContextConfiguration(classes = JenkinsConfig.class)
 public abstract class AbstractSeleniumTests extends AbstractTestNGSpringContextTests implements SeleniumTests
 {
-	private StringBuffer verificationErrors = new StringBuffer();
-
 	@AfterMethod
 	public void clearCookies()
 	{
 		getDriver().manage().deleteAllCookies();
 	}
 
-	@AfterSuite
+	@AfterClass
 	public void closeDriverObject()
 	{
-		getDriver().quit();
-		String verificationErrorString = verificationErrors.toString();
-		if (!"".equals(verificationErrorString))
-		{
-			fail(verificationErrorString);
-		}
+		getDriver().close();
 	}
 }
