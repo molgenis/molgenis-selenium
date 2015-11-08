d75641637bf85b6a94cfd13cb6c7b64718c350b3
Fleur Kelpin <f.kelpin@umcg.nl>
2015-11-08 14:32:18 +0100

    Adapt Select2Model for both single and multi select.

--- src/main/java/org/molgenis/selenium/model/component/Select2Model.java
+++ src/main/java/org/molgenis/selenium/model/component/Select2Model.java
@@ -1,22 +1,18 @@
 package org.molgenis.selenium.model.component;
 
 import static java.util.stream.Collectors.toList;
+import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import org.molgenis.selenium.util.SeleniumUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Quotes;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import junit.framework.Assert;
 
 /**
  * Model for a select2 mref selection box.
@@ -31,6 +27,9 @@ public class Select2Model implements MultiSelectModel
 	// Selector for the selected choices
 	private final By choiceSelector;
 	private final By selectedOptionSelector;
+	private final By selectSelector;
+	private boolean multi;
+	private final WebDriverWait tenSecondWait;
 
 	/**
 	 * Creates a new Select2Model
@@ -40,14 +39,17 @@ public class Select2Model implements MultiSelectModel
 	 * @param selector
 	 *            the id of the Select2, without the s2id_ prefix
 	 */
-	public Select2Model(WebDriver driver, String id)
+	public Select2Model(WebDriver driver, String id, boolean multi)
 	{
 		this.driver = driver;
 		this.id = id;
+		this.multi = multi;
 		closeButtonSelector = By
 				.xpath("//div[@id='s2id_" + id + "']//a[contains(@class, 'select2-search-choice-close')]");
 		choiceSelector = By.xpath("//div[@id='s2id_" + id + "']//.[contains(@class, 'select2-choice')]");
-		selectedOptionSelector = By.cssSelector("#s2id_" + id + " a.select2-choice");
+		selectedOptionSelector = By.cssSelector("#s2id_" + id + (multi ? " .select2-choices" : " .select2-choice"));
+		selectSelector = By.id("s2id_" + id);
+		tenSecondWait = new WebDriverWait(driver, 10);
 	}
 
 	/**
@@ -70,7 +72,7 @@ public class Select2Model implements MultiSelectModel
 	public void clearSelection()
 	{
 		LOG.info("Clear selection in Select2 with id {}...", id);
-		while (!driver.findElements(closeButtonSelector).isEmpty())
+		while (!StringUtils.isEmpty(driver.findElement(selectSelector).getText().trim()))
 		{
 			driver.findElement(closeButtonSelector).click();
 		}
@@ -85,7 +87,7 @@ public class Select2Model implements MultiSelectModel
 	 * @throws InterruptedException
 	 */
 	@Override
-	public void select(String... terms) throws InterruptedException
+	public void select(String... terms)
 	{
 		LOG.info("Selecting terms {} in Select2 with id {}...", Arrays.asList(terms), id);
 		for (String term : terms)
@@ -96,7 +98,7 @@ public class Select2Model implements MultiSelectModel
 
 			LOG.debug("Wait for text input box...");
 			WebElement select2InputText = driver
-					.findElement(By.xpath("//input[contains(@class, 'select2-input')][@type='text']"));
+					.findElement(By.cssSelector(multi ? "#s2id_" + id + " input" : "#select2-drop input"));
 
 			LOG.debug("Empty text input box..");
 			select2InputText.clear();
@@ -111,17 +113,8 @@ public class Select2Model implements MultiSelectModel
 			match.click();
 
 			LOG.debug("Waiting for selection to appear in the list of search choices...");
-
-			// Select select = new Select(driver.findElement(By.id(id)));
-			// SeleniumUtils.waitFor(() -> select.getAllSelectedOptions().stream().filter(e -> term.equals(e.getText()))
-			// .findFirst().isPresent());
-
-			// TODO:
-			// This worked for the multiple select case:
-			// select2InputText.sendKeys(Keys.ENTER);
-			// driver.findElement(By.xpath("//div[@id='s2id_" + id + "']//[contains(text(), '" + term + "')]"));
-
-			SeleniumUtils.waitFor(() -> select2Option.getText().trim().equals(term));
+			tenSecondWait.until(textToBePresentInElementLocated(
+					multi ? By.xpath("//div[@id='s2id_" + id + "']") : selectedOptionSelector, term));
 
 			LOG.debug("Selected term '{}'.", term);
 		}
