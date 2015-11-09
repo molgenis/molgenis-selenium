9e78ed97ef510e72308babe18b79fe337503cc13
Fleur Kelpin <f.kelpin@umcg.nl>
2015-11-09 23:35:20 +0100

    Smarter check for algorithm contents.

--- src/main/java/org/molgenis/selenium/model/mappingservice/AlgorithmEditorModel.java
+++ src/main/java/org/molgenis/selenium/model/mappingservice/AlgorithmEditorModel.java
@@ -1,16 +1,24 @@
 package org.molgenis.selenium.model.mappingservice;
 
+import org.apache.commons.lang3.StringUtils;
 import org.molgenis.selenium.model.AbstractModel;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AlgorithmEditorModel extends AbstractModel
 {
+	private static final Logger LOG = LoggerFactory.getLogger(AlgorithmEditorModel.class);
 
-	@FindBy(css = "textarea.ace_text-input")
-	WebElement algorithmTextarea;
+	@FindBy(css = "div.ace-editor-container")
+	WebElement aceEditorTextArea;
 
 	@FindBy(linkText = "Cancel and go back")
 	WebElement cancelAndGoBackButton;
@@ -20,9 +28,13 @@ public class AlgorithmEditorModel extends AbstractModel
 		super(driver);
 	}
 
-	public String getAlgorithm()
+	public AlgorithmEditorModel assertAlgorithmValueEquals(String string)
 	{
-		return algorithmTextarea.getText();
+		LOG.info("assertAlgorithmValueEquals {}...", string);
+		Wait<WebDriver> tenSecondWait = new WebDriverWait(driver, 10);
+		tenSecondWait.until(ExpectedConditions.textToBePresentInElement(aceEditorTextArea, string));
+		LOG.info("OK");
+		return this;
 	}
 
 	public MappingProjectDetailsModel cancelAndGoBack()
