6da657aa5390156fbbadcaaccb01d7a0d26d113e
Fleur Kelpin <f.kelpin@umcg.nl>
2015-11-12 20:27:06 +0100

    Wait until entity is selected.

--- src/test/java/org/molgenis/selenium/test/dataexplorer/DataExplorerTest.java
+++ src/test/java/org/molgenis/selenium/test/dataexplorer/DataExplorerTest.java
@@ -5,6 +5,7 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 import org.molgenis.JenkinsConfig;
 import org.molgenis.selenium.model.HomepageModel;
@@ -12,7 +13,9 @@ import org.molgenis.selenium.model.dataexplorer.DataExplorerModel;
 import org.molgenis.selenium.model.importer.ImporterModel;
 import org.molgenis.selenium.test.AbstractSeleniumTest;
 import org.molgenis.selenium.test.Config;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
@@ -58,6 +61,7 @@ public class DataExplorerTest extends AbstractSeleniumTest
 	@Test
 	public void test1() throws InterruptedException
 	{
+		LOG.info("Test data explorer, select TypeTest in entity select...");
 		// Test 1
 		model.selectEntity("TypeTest");
 		assertEquals(model.getSelectedEntityTitle(), "TypeTest");
@@ -71,10 +75,16 @@ public class DataExplorerTest extends AbstractSeleniumTest
 	@Test
 	public void test2() throws InterruptedException
 	{
+		LOG.info("Test data explorer, select TypeTest through URL...");
 		// Test 2
 		driver.get(baseURL + "/menu/main/dataexplorer?entity=org_molgenis_test_TypeTest");
-		assertEquals(model.getSelectedEntityTitle(), "TypeTest");
+		new WebDriverWait(driver, 2).pollingEvery(100, TimeUnit.MILLISECONDS).until(this::entityTypeTestIsSelected);
 		model.next();
 		assertTrue(driver.getCurrentUrl().endsWith("dataexplorer?entity=org_molgenis_test_TypeTest"));
+	}
+
+	private boolean entityTypeTestIsSelected(WebDriver d)
+	{
+		return "TypeTest".equals(model.getSelectedEntityTitle());
 	}
 }
