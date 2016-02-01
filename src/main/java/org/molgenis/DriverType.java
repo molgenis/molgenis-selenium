a94b3def0108db70691505b0df1797aa7efe7e21
Mark-de-Haan <markdehaan90@gmail.com>
2016-02-01 16:56:48 +0100

    Set standard resolution for the firefox driver to prevent layout induced selenium errors

--- src/main/java/org/molgenis/DriverType.java
+++ src/main/java/org/molgenis/DriverType.java
@@ -2,6 +2,7 @@ package org.molgenis;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -21,6 +22,7 @@ public enum DriverType implements DriverSetup
 		public WebDriver getWebDriverInstance(DesiredCapabilities capabilities)
 		{
 			FirefoxDriver driver = new FirefoxDriver(capabilities);
+			driver.manage().window().setSize(new Dimension(1920, 1080));
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			return driver;
 		}
